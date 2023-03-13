0938d565026b43eef3a2cdec2bafeb3b70a7cebc
Seth Morton <seth.m.morton@gmail.com>
2023-03-12 22:45:39 -0700

    Attempt to use const values for array lengths

--- locale-demo.c
+++ locale-demo.c
@@ -14,8 +14,10 @@ void compare_demo() {
     const wchar_t* wA = L"A";
 
     // Show comparison results for standard char
-    char xa[1 + strxfrm(NULL, a, 0)];
-    char xA[1 + strxfrm(NULL, A, 0)];
+    const size_t a_size = strxfrm(NULL, a, 0);
+    const size_t A_size = strxfrm(NULL, A, 0);
+    char xa[1 + a_size];
+    char xA[1 + A_size];
     strxfrm(xa, a, sizeof xa/sizeof *xa);
     strxfrm(xA, A, sizeof xA/sizeof *xA);
     printf("strcmp(\"a\", \"A\"): %d\n", strcmp(a, A));
@@ -23,8 +25,10 @@ void compare_demo() {
     printf("strcmp(strxfrm(\"a\"), strxfrm(\"A\")): %d\n", strcmp(xa, xA));
 
     // Show comparison results for wide char
-    wchar_t xwa[1 + wcsxfrm(NULL, wa, 0)];
-    wchar_t xwA[1 + wcsxfrm(NULL, wA, 0)];
+    const size_t wa_size = wcsxfrm(NULL, wa, 0);
+    const size_t wA_size = wcsxfrm(NULL, wA, 0);
+    wchar_t xwa[1 + wa_size];
+    wchar_t xwA[1 + wA_size];
     wcsxfrm(xwa, wa, sizeof xwa/sizeof *xwa);
     wcsxfrm(xwA, wA, sizeof xwA/sizeof *xwA);
     printf("wcscmp(\"a\", \"A\"): %d\n", wcscmp(wa, wA));
